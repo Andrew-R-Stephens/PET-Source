9e522854e9ba713cfb11fadec1e46c8cb21d3d5e
Andrew Stephens <tritiumgamingstudios@gmail.com>
2024-07-27 20:46:23 -0400

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,27 +33,28 @@ android {
         jvmToolchain(17)
     }
     /* ---------------- */
-    
+
     bundle {
         language {
             enableSplit = false
         }
     }
 
-    val playCoreDirectory = "G:\\Programs\\AndroidStudioRepositories\\play-core-native-sdk-1.10.0\\play-core-native-sdk"
+    val playCoreDirectory =
+        "G:\\Programs\\AndroidStudioRepositories\\play-core-native-sdk-1.10.0\\play-core-native-sdk"
 
     defaultConfig {
         applicationId = "com.TritiumGaming.phasmophobiaevidencepicker"
 
         minSdk = 23
         targetSdk = 34
-        versionCode = 110
-        versionName = "9.0.1.1"
+        versionCode = 111
+        versionName = "9.0.1.2"
 
         @Suppress("UnstableApiUsage")
         externalNativeBuild {
             cmake {
-                arguments ("-DANDROID_STL=c++_static", "-DPLAYCORE_LOCATION=$playCoreDirectory")
+                arguments("-DANDROID_STL=c++_static", "-DPLAYCORE_LOCATION=$playCoreDirectory")
             }
         }
         ndk {
@@ -70,19 +71,25 @@ android {
         debug {
             isMinifyEnabled = false
             isShrinkResources = false
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             // PLAYCORE
-            proguardFile ("$playCoreDirectory/proguard/common.pgcfg")
-            proguardFile ("$playCoreDirectory/proguard/per-feature-proguard-files")
+            proguardFile("$playCoreDirectory/proguard/common.pgcfg")
+            proguardFile("$playCoreDirectory/proguard/per-feature-proguard-files")
         }
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
 
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             // PLAYCORE
-            proguardFile ("$playCoreDirectory/proguard/common.pgcfg")
-            proguardFile ("$playCoreDirectory/proguard/per-feature-proguard-files")
+            proguardFile("$playCoreDirectory/proguard/common.pgcfg")
+            proguardFile("$playCoreDirectory/proguard/per-feature-proguard-files")
         }
     }
 
@@ -105,79 +112,79 @@ android {
 
 dependencies {
 
-    implementation (libs.firebase.analytics)
-    implementation (libs.androidx.core.ktx)
+    implementation(libs.firebase.analytics)
+    implementation(libs.androidx.core.ktx)
 
-    implementation (libs.android.support.multidex)
+    implementation(libs.android.support.multidex)
 
-    implementation (libs.kotlin.stdlib)
-    implementation (libs.google.gson)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.google.gson)
 
     // PRIMARY
-    implementation (libs.androidx.work.runtime)
-    implementation (libs.androidx.appcompat.core)
-    implementation (libs.androidx.constraintlayout)
-    implementation (libs.androidx.navigation.fragmentKtx)
-    implementation (libs.androidx.navigation.uiKtx)
-    implementation (libs.androidx.cardview.core)
-    implementation (libs.androidx.legacy.supportV4)
-    implementation (libs.android.material)
-    testImplementation (libs.junit)
-    androidTestImplementation (libs.androidx.testExt.junit)
-    androidTestImplementation (libs.androidx.espresso.core)
+    implementation(libs.androidx.work.runtime)
+    implementation(libs.androidx.appcompat.core)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragmentKtx)
+    implementation(libs.androidx.navigation.uiKtx)
+    implementation(libs.androidx.cardview.core)
+    implementation(libs.androidx.legacy.supportV4)
+    implementation(libs.android.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.testExt.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     // --- Google Credential Manager ----
     implementation(libs.androidx.credentials.core)
     implementation(libs.androidx.credentials.playServicesAuth)
-    implementation (libs.firebaseUi.auth)
+    implementation(libs.firebaseUi.auth)
     // Phillip Lackner suggested for OTC (one-tap client)
-    implementation (libs.firebase.authKtx)
-    implementation (libs.android.playServices.auth)
+    implementation(libs.firebase.authKtx)
+    implementation(libs.android.playServices.auth)
     // ----------------------------------
 
     // GOOGLE FIREBASE
     // Import the BoM for the Firebase platform
-    implementation (libs.firebase.auth)
-    implementation (platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(platform(libs.firebase.bom))
     // GOOGLE FIREBASE FIRESTORE
-    implementation (libs.firebase.firestore)
+    implementation(libs.firebase.firestore)
     // GOOGLE BILLING API
-    implementation (libs.android.billing.core)
+    implementation(libs.android.billing.core)
 
 
     // Declare the dependencies for the Crashlytics and Analytics libraries
     // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation (libs.firebase.crashlyticsCore)
-    implementation (libs.firebase.analytics)
-    implementation (libs.firebase.perfCore)
+    implementation(libs.firebase.crashlyticsCore)
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.perfCore)
     // GOOGLE ADS
-    implementation (libs.android.playServices.ads)
-    implementation (libs.android.ump.core)
+    implementation(libs.android.playServices.ads)
+    implementation(libs.android.ump.core)
 
     //GOOGLE GSON
-    implementation (libs.google.gson)
+    implementation(libs.google.gson)
 
     // IN-APP REVIEWS
     /* This dependency is downloaded from the Google’s Maven repository.
      * So, make sure you also include that repository in your project's build.gradle file.*/
-    implementation (libs.android.play.core.review)
+    implementation(libs.android.play.core.review)
     // For Kotlin users also add the Kotlin extensions library for Play Core:
-    implementation (libs.android.play.coreKtx.review)
+    implementation(libs.android.play.coreKtx.review)
 
     // IN-APP UPDATES
     /* This dependency is downloaded from the Google’s Maven repository.
      * So, make sure you also include that repository in your project's build.gradle file.*/
-    implementation (libs.android.play.core.update)
+    implementation(libs.android.play.core.update)
     // For Kotlin users also add the Kotlin extensions library for Play Core:
-    implementation (libs.android.play.coreKtx.update)
+    implementation(libs.android.play.coreKtx.update)
 
     // FRAGMENTS
     // Java language implementation
     // implementation libs.androidx.fragment
-    implementation (libs.androidx.fragmentKtx)
+    implementation(libs.androidx.fragmentKtx)
 
     // For use with animated gifs
-    implementation (libs.droidsonroids.gifDrawable)
+    implementation(libs.droidsonroids.gifDrawable)
 
     // DO NOT UPDATE THIS -- BREAKING CHANGES
     //implementation (libs.commonsIo.core)
@@ -186,56 +193,56 @@ dependencies {
         ---- START----
         ANDROIDX LEANBACK
     */
-    implementation (libs.androidx.leanback.core)
+    implementation(libs.androidx.leanback.core)
     // leanback-preference is an add-on that provides a settings UI for TV apps.
-    implementation (libs.androidx.leanback.preference)
+    implementation(libs.androidx.leanback.preference)
     // leanback-paging is an add-on that simplifies adding paging support to a RecyclerView Adapter.
-    implementation (libs.androidx.leanback.paging)
+    implementation(libs.androidx.leanback.paging)
     // leanback-tab is an add-on that provides customized TabLayout to be used as the top navigation bar.
-    implementation (libs.androidx.leanback.tab)
+    implementation(libs.androidx.leanback.tab)
 
     /*
         ---- START----
         ANDROID COMPOSE
     */
-    implementation (platform(libs.androidx.compose.bom))
+    implementation(platform(libs.androidx.compose.bom))
     //implementation (libs.androidx.compose.animation)
-    androidTestImplementation (platform(libs.androidx.compose.bom))
-    androidTestImplementation (libs.androidx.composeUi.testJunit4)
-    debugImplementation (libs.androidx.composeUi.testManifest)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.composeUi.testJunit4)
+    debugImplementation(libs.androidx.composeUi.testManifest)
 
     // Material Design 3
-    implementation (libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3)
     // Android Studio Preview support
-    implementation (libs.androidx.composeUi.toolingPreview)
-    debugImplementation (libs.androidx.composeUi.tooling)
+    implementation(libs.androidx.composeUi.toolingPreview)
+    debugImplementation(libs.androidx.composeUi.tooling)
 
     // Optional - Included automatically by material, only add when you need
     // the icons but not the material library (e.g. when using Material3 or a
     // custom design system based on Foundation)
-    implementation (libs.androidx.compose.materialIconsCore)
+    implementation(libs.androidx.compose.materialIconsCore)
     // Optional - Add full set of material icons
-    implementation (libs.androidx.compose.materialIconsExtended)
+    implementation(libs.androidx.compose.materialIconsExtended)
     // Optional - Add window size utils
-    implementation (libs.androidx.compose.material3WindowSizeClass)
+    implementation(libs.androidx.compose.material3WindowSizeClass)
     // Optional - Integration with LiveData
-    implementation (libs.androidx.compose.runtime.liveData)
+    implementation(libs.androidx.compose.runtime.liveData)
     // Optional - Integration with RxJava
-    implementation (libs.androidx.compose.runtime.rxJava2)
+    implementation(libs.androidx.compose.runtime.rxJava2)
 
-    implementation (libs.androidx.composeUi.core)
-    implementation (libs.androidx.composeMaterial.core)
+    implementation(libs.androidx.composeUi.core)
+    implementation(libs.androidx.composeMaterial.core)
 
-    implementation (libs.androidx.activityCompose)
-    implementation (libs.androidx.lifecycle.runtimeKtx)
-    implementation (libs.androidx.lifecycle.viewmodelCompose)
-    implementation (libs.androidx.lifecycle.runtimeCompose)
-    implementation (libs.androidx.navigation.compose)
+    implementation(libs.androidx.activityCompose)
+    implementation(libs.androidx.lifecycle.runtimeKtx)
+    implementation(libs.androidx.lifecycle.viewmodelCompose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
 
-    implementation (libs.coilKt.compose)
+    implementation(libs.coilKt.compose)
 
     // DataStore
-    implementation (libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.preferences)
 
     /*
         ----- END -----
@@ -244,7 +251,7 @@ dependencies {
     /*
      * Realms DB
      */
-    implementation (libs.kotlinx.coroutines)
-    implementation (libs.realmDb.kotlin.libraryCore)
-    implementation (libs.realmDb.kotlin.librarySync)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.realmDb.kotlin.libraryCore)
+    implementation(libs.realmDb.kotlin.librarySync)
 }
