42672a1fa8a0bb566f7bc9219dfc47e1a16701cd
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-06-12 20:26:10 -0400

    Refactor: Introduce domain model for WorldMaps
    This commit introduces a `WorldMaps` domain model and updates the `ComplexMapRepositoryImpl` to map the `WorldMapsDto` to this new domain model.
    
    A `toDomain()` extension function has been added to `WorldMapsDto.kt` to handle this conversion.
    
    The `ComplexMapRepositoryImpl` now calls `toDomain()` after mapping the fetched data to `WorldMapsDto`, ensuring that the repository returns the domain model instead of the DTO.

---
+++ app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/TestComplexMaps.kt
@@ -0,0 +1,52 @@
+package com.tritiumgaming.phasmophobiaevidencepicker
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.repository.ComplexMapRepositoryImpl
+import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.source.local.ComplexMapLocalDataSource
+import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.source.local.ComplexMapLocalService
+import com.tritiumgaming.phasmophobiaevidencepicker.operation.domain.map.complex.usecase.FetchComplexMapsUseCase
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class TestComplexMaps {
+
+    private lateinit var mockContext: Context
+
+    private lateinit var complexMapService: ComplexMapLocalService
+    private lateinit var complexMapLocalDataSource: ComplexMapLocalDataSource
+    private lateinit var complexMapsRepository: ComplexMapRepositoryImpl
+    private lateinit var fetchComplexMapsUseCase: FetchComplexMapsUseCase
+
+    private val testDispatcher = StandardTestDispatcher()
+
+
+    @Before
+    fun setUp() {
+        mockContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        complexMapService = ComplexMapLocalService()
+        complexMapLocalDataSource = ComplexMapLocalDataSource(
+            applicationContext = mockContext,
+            service = complexMapService
+        )
+        complexMapsRepository = ComplexMapRepositoryImpl(
+            localSource = complexMapLocalDataSource
+        )
+        fetchComplexMapsUseCase = FetchComplexMapsUseCase(complexMapsRepository)
+    }
+
+    @Test
+    fun fetchComplexMaps(): Unit =
+        runTest(testDispatcher) {
+            val result = complexMapsRepository.fetchMaps()
+
+            assertTrue("${ result.exceptionOrNull()?.message }", result.isSuccess)
+
+            println(result.getOrNull())
+            result.getOrNull()?.print()
+        }
+}
