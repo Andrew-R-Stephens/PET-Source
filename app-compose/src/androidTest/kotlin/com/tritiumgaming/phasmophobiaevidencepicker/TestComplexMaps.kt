1b4114a532f770bd846d2f6cdd37c9dad542119c
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-06-13 23:22:03 -0400

    test: Import `ComplexMapLocalService` from correct package
    The `ComplexMapLocalService` class was moved to a subpackage. This commit updates the import statement in `TestComplexMaps.kt` to reflect the new location.

--- app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/TestComplexMaps.kt
+++ app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/TestComplexMaps.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.repository.ComplexMapRepositoryImpl
 import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.source.local.ComplexMapLocalDataSource
-import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.source.local.ComplexMapLocalService
+import com.tritiumgaming.phasmophobiaevidencepicker.operation.data.map.complex.source.local.service.ComplexMapLocalService
 import com.tritiumgaming.phasmophobiaevidencepicker.operation.domain.map.complex.usecase.FetchComplexMapsUseCase
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.test.StandardTestDispatcher
