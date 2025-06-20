1d1ec3f1e5df0315001e533a133b952cd2559cc2
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-06-20 05:10:27 -0400

    refactor: Remove GlobalPreferences related files
    The `GlobalPreferences` functionality is no longer needed. Additionally, unused imports were removed from `DatastoreDataSource.kt`.

--- shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/core/domain/datastore/DatastoreDataSource.kt
+++ shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/core/domain/datastore/DatastoreDataSource.kt
@@ -1,9 +1,5 @@
 package com.tritiumgaming.phasmophobiaevidencepicker.core.domain.datastore
 
-import androidx.datastore.preferences.core.Preferences
-import androidx.lifecycle.liveData
-import kotlinx.coroutines.flow.Flow
-
 interface DatastoreDataSource<T> {
 
     suspend fun initFlow(onUpdate: (T) -> Unit)
