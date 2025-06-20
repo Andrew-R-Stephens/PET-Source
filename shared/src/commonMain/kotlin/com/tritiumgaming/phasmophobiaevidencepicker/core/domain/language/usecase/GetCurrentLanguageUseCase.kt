f2afcc7f34fca79ef6105560c76dc20d65175834
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-06-20 05:10:48 -0400

    refactor: Remove unused language-related files and classes
    This commit removes a number of files and classes related to language selection and management that are no longer in use.

--- shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/core/domain/language/usecase/GetCurrentLanguageUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.tritiumgaming.phasmophobiaevidencepicker.core.domain.language.usecase
-
-import com.tritiumgaming.phasmophobiaevidencepicker.core.domain.language.repository.LanguageRepository
-
-class GetCurrentLanguageUseCase(
-    private val repository: LanguageRepository
-) {
-    operator fun invoke() = repository.getCurrentLanguageCode()
-}
