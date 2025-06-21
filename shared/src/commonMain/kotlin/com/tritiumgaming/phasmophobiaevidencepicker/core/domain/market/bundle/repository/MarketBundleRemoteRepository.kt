bf1efd9f511c09c2734fd4eb7d98ee5f0f156f18
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-06-20 20:17:53 -0400

    feat: Add market domain models and query options
    This commit introduces several new domain models and query option classes for the market feature, and moves existing market-related domain files from the `app-compose` module to the `shared` module.

--- shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/core/domain/market/bundle/repository/MarketBundleRemoteRepository.kt
+++ shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/core/domain/market/bundle/repository/MarketBundleRemoteRepository.kt
@@ -1,6 +1,6 @@
 package com.tritiumgaming.phasmophobiaevidencepicker.core.domain.market.bundle.repository
 
-import com.tritiumgaming.phasmophobiaevidencepicker.core.data.market.bundle.source.remote.MarketBundleFirestoreDataSourceImpl.BundleQueryOptions
+import com.tritiumgaming.phasmophobiaevidencepicker.core.domain.market.bundle.model.BundleQueryOptions
 import com.tritiumgaming.phasmophobiaevidencepicker.core.domain.market.bundle.model.MarketBundle
 
 interface MarketBundleRemoteRepository {
