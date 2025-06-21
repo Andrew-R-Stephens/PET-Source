facc63dc0072d33fe5a85f1e340f8c97de118ddb
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-06-20 20:18:40 -0400

    feat: Implement platform-specific QueryOptions for market items
    This commit introduces platform-specific implementations
    
    These classes provide constructors for default and custom query parameters, including filtering, ordering, and limiting results. This allows for more flexible and platform-aware data fetching from Firestore for different market item types.

---
+++ shared/src/iosMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/core/domain/market/billable/model/BillableQueryOptions.kt
@@ -0,0 +1,56 @@
+package com.tritiumgaming.phasmophobiaevidencepicker.core.domain.market.billable.model
+
+actual class BillableQueryOptions {
+
+    actual constructor() {
+        this.filterField = FilterField.NONE
+        this.filterValue = FilterValue.NONE
+        this.orderField = OrderField.NONE
+        this.orderDirection = OrderDirection.DESCENDING
+        this.limit = Limit.UNLIMITED
+    }
+
+    actual constructor(
+        filterField: FilterField?,
+        filterValue: FilterValue?,
+        orderField: OrderField?,
+        orderDirection: OrderDirection?,
+        limit: Limit?
+    ) {
+        this.filterField = filterField ?: FilterField.NONE
+        this.filterValue = filterValue ?: FilterValue.NONE
+        this.orderField = orderField ?: OrderField.NONE
+        this.orderDirection = orderDirection ?: OrderDirection.DESCENDING
+        this.limit = limit ?: Limit.SAFE_LIMIT
+    }
+
+    actual val filterField: FilterField
+    actual val filterValue: FilterValue
+    actual val orderField: OrderField
+    actual val orderDirection: OrderDirection
+    actual val limit: Limit
+
+
+    actual enum class FilterField(val value: String?) {
+        TYPE("type"),
+        NONE(null)
+    }
+    actual enum class FilterValue(val value: String?) {
+        CREDITS("credits"),
+        NONE(null)
+    }
+    actual enum class OrderField(val value: String?) {
+        NONE(null)
+    }
+
+    // TODO: Change to iOS firebase firestore ordering
+    // possible library import will be com.google.firebase.firestore.Query
+    actual enum class OrderDirection(val value: Int) {
+        ASCENDING(1),
+        DESCENDING(2)
+    }
+    actual enum class Limit(val value: Int) {
+        SAFE_LIMIT(50),
+        UNLIMITED(Int.MAX_VALUE)
+    }
+}
