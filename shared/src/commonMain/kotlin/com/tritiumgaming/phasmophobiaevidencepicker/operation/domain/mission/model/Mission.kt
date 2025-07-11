65de9cb803f7da2ac7d865170d44e6c44ab8f6c8
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-07-11 04:35:28 -0400

    Remove deprecated MissionListModel and update Mission model
    This commit removes the deprecated `MissionsListModel` class as it is no longer in use.
    
    The `Mission` data class has been updated:
    - The `mission` property has been renamed to `content`.
    - The `missionId` property has been renamed to `id` and its type changed to `String`.
    - The `selected` property and associated `select()` and `deselect()` methods have been removed.
    - The `getData()` method has been removed.

--- shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/operation/domain/mission/model/Mission.kt
+++ shared/src/commonMain/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/operation/domain/mission/model/Mission.kt
@@ -3,23 +3,6 @@ package com.tritiumgaming.phasmophobiaevidencepicker.operation.domain.mission.mo
 import com.tritiumgaming.phasmophobiaevidencepicker.operation.domain.mission.mappers.MissionResources.MissionContent
 
 class Mission(
-    val mission: MissionContent,
-    var missionId: Int? = null
-) {
-    var selected: Boolean = false
-
-    fun select(missionId: Int) {
-        selected = true
-        this.missionId = missionId
-    }
-
-    fun deselect() {
-        selected = false
-        missionId = null
-    }
-
-    fun getData(): String {
-        return "$selected $missionId $mission"
-    }
-
-}
+    val content: MissionContent,
+    var id: String
+)
