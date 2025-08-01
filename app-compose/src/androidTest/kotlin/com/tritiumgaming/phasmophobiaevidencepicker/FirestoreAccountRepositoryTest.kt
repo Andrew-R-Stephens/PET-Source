42105d767c86f81c5f58fb303a96e1bd76213465
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-08-01 14:54:56 -0400

    Refactor: Update Firebase KTX imports

--- app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/FirestoreAccountRepositoryTest.kt
+++ app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/FirestoreAccountRepositoryTest.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.Firebase
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import org.junit.After
