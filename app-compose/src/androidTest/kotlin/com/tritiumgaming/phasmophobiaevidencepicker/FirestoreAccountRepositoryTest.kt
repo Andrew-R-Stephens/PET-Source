af6e00f401d8312f75548f33dc3ecf92f0d82db1
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-08-01 15:01:39 -0400

    Refactor: Update Firebase import order

--- app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/FirestoreAccountRepositoryTest.kt
+++ app-compose/src/androidTest/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/FirestoreAccountRepositoryTest.kt
@@ -2,12 +2,12 @@ package com.tritiumgaming.phasmophobiaevidencepicker
 
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
-import com.google.firebase.Firebase
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import org.junit.After
