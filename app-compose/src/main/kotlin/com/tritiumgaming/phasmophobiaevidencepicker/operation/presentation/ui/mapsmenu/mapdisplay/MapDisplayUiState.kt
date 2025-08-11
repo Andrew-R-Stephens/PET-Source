0977ff28354f507f1fcb016ce0bad28adb44c5ca
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-08-11 15:47:16 -0400

    Refactor: Remove unused MapDisplayUiState

--- app-compose/src/main/kotlin/com/tritiumgaming/phasmophobiaevidencepicker/operation/presentation/ui/mapsmenu/mapdisplay/MapDisplayUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.tritiumgaming.phasmophobiaevidencepicker.operation.presentation.ui.mapsmenu.mapdisplay
-
-data class MapDisplayUiState(
-    val mapId: String = "",
-    val floorIndex: Int = 0,
-    val roomId: Int = 0
-)
