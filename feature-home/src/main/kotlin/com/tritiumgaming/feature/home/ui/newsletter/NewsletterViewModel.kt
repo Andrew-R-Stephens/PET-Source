9a02ff3d77969f4c81617808cc6644fbe96d818d
Andrew Stephens <tritiumgamingstudios@gmail.com>
2025-10-24 18:47:09 -0400

    refactor(newsletter): Refactor ViewModel to use Combine and StateFlow
    The `NewsletterViewModel` has been updated to combine multiple flows (`getFlowNewsletterInboxesUseCase` and `getFlowNewsletterDatastoreUseCase`) into a single `StateFlow` for the UI state.
    
    This change simplifies state management by removing manual updates with `MutableStateFlow.update` and leverages `stateIn` for more efficient data observation. The logic for loading and saving data has been streamlined accordingly.

--- feature-home/src/main/kotlin/com/tritiumgaming/feature/home/ui/newsletter/NewsletterViewModel.kt
+++ feature-home/src/main/kotlin/com/tritiumgaming/feature/home/ui/newsletter/NewsletterViewModel.kt
@@ -3,37 +3,114 @@ package com.tritiumgaming.feature.home.ui.newsletter
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.tritiumgaming.feature.home.app.container.HomeContainerProvider
 import com.tritiumgaming.shared.home.domain.newsletter.model.NewsletterInbox
+import com.tritiumgaming.shared.home.domain.newsletter.source.NewsletterDatastore
 import com.tritiumgaming.shared.home.domain.newsletter.usecase.FetchNewsletterInboxesUseCase
-import com.tritiumgaming.shared.home.domain.newsletter.usecase.InitFlowNewsletterUseCase
+import com.tritiumgaming.shared.home.domain.newsletter.usecase.GetFlowNewsletterDatastoreUseCase
+import com.tritiumgaming.shared.home.domain.newsletter.usecase.GetFlowNewsletterInboxesUseCase
 import com.tritiumgaming.shared.home.domain.newsletter.usecase.SaveNewsletterInboxLastReadDateUseCase
 import com.tritiumgaming.shared.home.domain.newsletter.usecase.SetupNewsletterUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
 class NewsletterViewModel(
     private val setupNewsletterDatastoreUseCase: SetupNewsletterUseCase,
-    private val initFlowNewsletterUseCase: InitFlowNewsletterUseCase,
+    private val getFlowNewsletterDatastoreUseCase: GetFlowNewsletterDatastoreUseCase,
+    private val getFlowNewsletterInboxesUseCase: GetFlowNewsletterInboxesUseCase,
     private val fetchNewsletterInboxesUseCase: FetchNewsletterInboxesUseCase,
     private val saveNewsletterInboxLastReadDateUseCase: SaveNewsletterInboxLastReadDateUseCase,
 ): ViewModel() {
 
-    private val _inboxesUiState = MutableStateFlow<NewsletterInboxesUiState>(
+    /*private val _inboxesUiState = MutableStateFlow<NewsletterInboxesUiState>(
         NewsletterInboxesUiState())
-    val inboxesUiState = _inboxesUiState.asStateFlow()
+    val inboxesUiState = _inboxesUiState.asStateFlow()*/
+
+    private val _inboxesUiState = getFlowNewsletterInboxesUseCase()
+        .combine(getFlowNewsletterDatastoreUseCase()) { inboxes, datastore ->
+            Log.d("NewsletterViewModel",
+                "Fetched remote flow:\n ${inboxes.map { 
+                    "${it.title}: ${it.channel?.messages?.map { message -> message.title }}\n"}}")
+            Log.d("NewsletterViewModel",
+                "Fetched datastore:\n ${datastore.data.entries.map { 
+                    "${it.key} -> ${it.value}\n"}}")
+
+            NewsletterInboxesUiState(
+                inboxes = inboxes.map { inbox ->
+                    NewsletterInboxUiState(
+                        inbox = inbox,
+                        lastReadDate = datastore.data.entries.firstOrNull {
+                            it.key == inbox.id
+                        }?.value ?: 0L
+                    )
+                }
+            )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = NewsletterInboxesUiState(
+                inboxes = emptyList()
+            )
+        )
+    val inboxesUiState = _inboxesUiState
+
+    /*private val _newsletterPreferencesState =
+        getFlowNewsletterDatastoreUseCase()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = NewsletterDatastore.NewsletterPreferences(
+                    data = emptyMap()
+                )
+            )
+    val newsletterPreferencesState = _newsletterPreferencesState*/
 
     private val _refreshUiState = MutableStateFlow<NewsletterRefreshingUiState>(
         NewsletterRefreshingUiState())
     val refreshUiState = _refreshUiState.asStateFlow()
 
     fun saveInboxLastReadDate(inboxId: String, date: Long) {
+        viewModelScope.launch {
+            saveNewsletterInboxLastReadDateUseCase(inboxId, date)
+        }
+    }
+
+    fun loadInboxes(
+        onStart: () -> Unit = {},
+        onComplete: () -> Unit = {},
+    ) {
+
+        viewModelScope.launch {
+
+            try {
+                val fetchedInboxes = fetchNewsletterInboxesUseCase().getOrThrow()
+
+                fetchedInboxes.forEach { inbox ->
+                    Log.d("NewsletterViewModel",
+                        "Fetched inbox: ${inbox.title} ${inbox.channel?.messages?.map { "${ it.title }\n" }}")
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    /*fun saveInboxLastReadDate(inboxId: String, date: Long) {
         viewModelScope.launch {
 
             try {
@@ -76,9 +153,9 @@ class NewsletterViewModel(
             Log.d("NewsletterViewModel", "Updated inbox dates: ${inboxesUiState.value.inboxes.map { it.lastReadDate }}")
 
         }
-    }
+    }*/
 
-    fun loadInboxes(
+    /*fun loadInboxes(
         onStart: () -> Unit = {},
         onComplete: () -> Unit = {},
     ) {
@@ -101,7 +178,7 @@ class NewsletterViewModel(
                     )
                 }
 
-                initFlowNewsletterUseCase { preferences ->
+                getFlowNewsletterDatastoreUseCase { preferences ->
                     Log.d("NewsletterViewModel", "Datastore flow triggered")
 
                     _inboxesUiState.update { inboxes ->
@@ -117,7 +194,6 @@ class NewsletterViewModel(
                     }
 
                     onComplete()
-
                 }
 
             } catch (e: Exception) {
@@ -127,7 +203,7 @@ class NewsletterViewModel(
             onComplete()
 
         }
-    }
+    }*/
 
     fun refreshInboxes(
         onSuccess: () -> Unit = {},
@@ -181,7 +257,8 @@ class NewsletterViewModel(
 
                 NewsletterViewModel(
                     setupNewsletterDatastoreUseCase = container.setupNewsletterUseCase,
-                    initFlowNewsletterUseCase = container.initFlowNewsletterUseCase,
+                    getFlowNewsletterDatastoreUseCase = container.getFlowNewsletterDatastoreUseCase,
+                    getFlowNewsletterInboxesUseCase = container.getFlowNewsletterInboxesUseCase,
                     fetchNewsletterInboxesUseCase = container.getNewsletterInboxesUseCase,
                     saveNewsletterInboxLastReadDateUseCase = container.saveNewsletterInboxLastReadDateUseCase
                 )
