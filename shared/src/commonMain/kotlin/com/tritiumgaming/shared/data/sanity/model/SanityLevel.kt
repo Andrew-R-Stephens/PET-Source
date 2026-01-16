02fab969963c64ba23dd5834439809dec83964d3
Andrew Stephens <tritiumgamingstudios@gmail.com>
2026-01-16 12:23:57 -0500

    refactor: simplify typography retrieval in PETActivityViewModel
    Slightly refactors `getTypographyByUUID` to assign the result of `getTypographyByUUIDUseCase` to a local variable before converting it to a typography resource.

---
+++ shared/src/commonMain/kotlin/com/tritiumgaming/shared/data/sanity/model/SanityLevel.kt
@@ -0,0 +1,12 @@
+package com.tritiumgaming.shared.data.sanity.model
+
+object SanityLevel {
+
+    const val MIN_SANITY = 0.0f
+    const val HALF_SANITY = 0.5f
+    const val THREE_FOURTH_SANITY = 0.75f
+    const val MAX_SANITY = 1.0f
+
+    const val SAFE_MIN_BOUNDS = 0.7f
+
+}
