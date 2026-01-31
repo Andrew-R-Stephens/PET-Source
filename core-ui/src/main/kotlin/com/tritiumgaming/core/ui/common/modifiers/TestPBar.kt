aef5274837fea39900aeb8a5465103ca86d7410b
Andrew Stephens <tritiumgamingstudios@gmail.com>
2026-01-30 22:28:10 -0500

    refactor: reorganize and expand custom Compose modifiers
    Refactors the custom modifiers within `core-ui` by splitting them into dedicated files and adding a new layout modifier.
    
    Key changes include:
    - Renaming `CustomModifiers.kt` to `FadingEdge.kt` to better reflect its primary content.
    - Moving the `progressGradient` modifier and its corresponding `TestPBar` preview to a new file, `TestPBar.kt`.
    - Adding a new `offsetPercent` modifier in `OffsetPercent.kt` to allow positioning elements based on a percentage of their own dimensions.

---
+++ core-ui/src/main/kotlin/com/tritiumgaming/core/ui/common/modifiers/TestPBar.kt
@@ -0,0 +1,55 @@
+package com.tritiumgaming.core.ui.common.modifiers
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+fun Modifier.progressGradient(
+    progress: Float = .5f,
+    gradientColor: Color = Color.Red,
+    gradientAlpha: Float = 1f
+): Modifier = this.then(
+    Modifier
+        // adding layer fixes issue with blending gradient and content
+        .graphicsLayer { alpha = gradientAlpha }
+        .drawWithContent {
+
+            val gradientLength = 32f / size.width
+
+            drawRect(
+                brush = Brush.horizontalGradient(
+                    -gradientLength to gradientColor,
+                    progress to gradientColor,
+                    progress+gradientLength to Color.Transparent,
+                    startX = -gradientLength,
+                    endX = size.width
+                ),
+                blendMode = BlendMode.SrcOver
+            )
+
+            drawContent()
+        }
+)
+
+@Composable
+@Preview
+fun TestPBar() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(48.dp)
+        .progressGradient(
+            progress = 1f,
+            gradientColor = Color.Blue,
+            gradientAlpha = .5f
+        )
+    )
+}
