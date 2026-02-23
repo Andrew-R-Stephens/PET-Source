1aaa6cea0043702c645ec1ca710d855f277f58fd
Andrew Stephens <tritiumgamingstudios@gmail.com>
2026-02-22 19:12:37 -0500

    style: refactor `NavRoute` enum to use named arguments
    Updates the `NavRoute` enum to use explicit named arguments for the `route` property across all navigation and screen definitions to improve code clarity and maintain consistency.

--- shared/src/androidMain/kotlin/com/tritiumgaming/shared/core/navigation/NavRoute.kt
+++ shared/src/androidMain/kotlin/com/tritiumgaming/shared/core/navigation/NavRoute.kt
@@ -3,35 +3,35 @@ package com.tritiumgaming.shared.core.navigation
 enum class NavRoute(val route: String) {
 
     // Home Navigation
-    NAVIGATION_HOME("HomeNavigation"),
-    NAVIGATION_MARKETPLACE("MarketplaceNavigation"),
-    NAVIGATION_NEWSLETTER("NewsletterNavigation"),
+    NAVIGATION_HOME(route = "HomeNavigation"),
+    NAVIGATION_MARKETPLACE(route = "MarketplaceNavigation"),
+    NAVIGATION_NEWSLETTER(route = "NewsletterNavigation"),
 
-    SCREEN_ACCOUNT_OVERVIEW("AccountScreen"),
-    SCREEN_APP_INFO("InfoScreen"),
-    SCREEN_LANGUAGE("LanguageScreen"),
-    SCREEN_SETTINGS("SettingsScreen"),
-    SCREEN_HOME("HomeScreen"),
+    SCREEN_ACCOUNT_OVERVIEW(route = "AccountScreen"),
+    SCREEN_APP_INFO(route = "InfoScreen"),
+    SCREEN_LANGUAGE(route = "LanguageScreen"),
+    SCREEN_SETTINGS(route = "SettingsScreen"),
+    SCREEN_HOME(route = "HomeScreen"),
 
-    SCREEN_NEWSLETTER_INBOX("NewsInboxScreen"),
-    SCREEN_NEWSLETTER_MESSAGES("NewsMessagesScreen"),
-    SCREEN_NEWSLETTER_MESSAGE("NewsMessageScreen"),
+    SCREEN_NEWSLETTER_INBOX(route = "NewsInboxScreen"),
+    SCREEN_NEWSLETTER_MESSAGES(route = "NewsMessagesScreen"),
+    SCREEN_NEWSLETTER_MESSAGE(route = "NewsMessageScreen"),
 
-    SCREEN_MARKETPLACE_UNLOCKS("MarketplaceUnlocksScreen"),
-    SCREEN_MARKETPLACE_BILLABLE("MarketplaceBillableScreen"),
+    SCREEN_MARKETPLACE_UNLOCKS(route = "MarketplaceUnlocksScreen"),
+    SCREEN_MARKETPLACE_BILLABLE(route = "MarketplaceBillableScreen"),
 
     // Operation Navigation
-    NAVIGATION_INVESTIGATION("OperationNavigation"),
-    NAVIGATION_MAPS ("MapsNavigation"),
-    NAVIGATION_CODEX ("CodexNavigation"),
+    NAVIGATION_INVESTIGATION(route = "OperationNavigation"),
+    NAVIGATION_MAPS (route = "MapsNavigation"),
+    NAVIGATION_CODEX (route = "CodexNavigation"),
 
-    SCREEN_INVESTIGATION("InvestigationScreen"),
-    SCREEN_MISSIONS("MissionsScreen"),
-    SCREEN_MAPS_MENU("MapMenuScreen"),
-    SCREEN_MAP_VIEWER("MapViewerScreen"),
+    SCREEN_INVESTIGATION(route = "InvestigationScreen"),
+    SCREEN_MISSIONS(route = "MissionsScreen"),
+    SCREEN_MAPS_MENU(route = "MapMenuScreen"),
+    SCREEN_MAP_VIEWER(route = "MapViewerScreen"),
 
-    SCREEN_CODEX_MENU("CodexMenuScreen"),
-    SCREEN_CODEX_ITEM_SCREEN("CodexItemstoreScreen"),
+    SCREEN_CODEX_MENU(route = "CodexMenuScreen"),
+    SCREEN_CODEX_ITEM_SCREEN(route = "CodexItemstoreScreen"),
 
 
 }
